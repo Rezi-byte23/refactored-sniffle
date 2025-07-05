6e9bb32b74966496f2e6fd1fc76d65e0aac1f87d
Rezi-byte23 <wendyachmadfahrezimicrosoftacc@gmail.com>
2025-07-06 05:05:19 +0700

    Add files via upload

---
+++ home.php
@@ -0,0 +1,409 @@
+
+
+<html>
+<head>
+	<title>[SIAKAD-SP] Selamat Datang</title>
+  <link rel = "icon" type = "image" href = "favicon.png">
+	<meta http-equiv="content-type" content="text/html;charset=iso-8859-1">
+
+  <link rel="stylesheet" href="pager.css">
+	
+</head>
+<body leftmargin="0" rightmargin="0" topmargin="0" bottommargin="0">
+
+
+<style>
+.table th {
+   
+   vertical-align: middle;
+   border: none;
+}
+.icon-circle:hover {
+    background-color: rgba(255, 255, 255, 1); /* Warna saat hover */
+}
+   html, body {
+    height: 100%; /* Pastikan body mengambil seluruh tinggi halaman */
+    display: flex;
+    flex-direction: column; /* Mengatur arah flex menjadi kolom */
+}
+
+body {
+ /* Atur lebar body */
+ 
+  flex: 1;
+    font-family: Verdana, Arial, Helvetica, sans-serif;
+    font-size: 0.9em;
+    margin: 0; /* Hapus margin default */
+    padding: 0; 
+}
+.card {
+    width: 100px; /* Kecilkan ukuran card */
+    margin-bottom: 10px;
+    border: none;
+}
+
+.table {
+    font-size: 0.7em; /* Kecilkan ukuran font di dalam tabel */
+    width: 100%; /* Kecilkan lebar tabel */
+    margin: 0; /* Hapus margin */
+            padding: 0; /* Hapus padding */
+      
+            
+          
+}
+.table td {
+   
+   vertical-align: middle; /* Menempatkan teks di tengah secara vertikal */
+}
+.table tr:nth-child(even) td {
+  background: #e3ebef;/* Warna abu-abu untuk baris genap */
+}
+
+.table tr:nth-child(odd) {
+    background-color: #ffffff; /* Warna putih untuk baris ganjil */
+}
+
+        .card-header {
+            padding: 5px; /* Kurangi padding pada header card */
+            text-align: center;
+        }
+
+        .text-start {
+          margin-left: 5px; /* Kurangi margin kiri pada teks */
+        }
+        .table tr:hover {
+    background-color: orange; /* Warna oranye saat hover */
+}
+        .bg-dodger-blue {
+        background-color: dodgerblue; /* Warna Dodger Blue */
+        color: white; /* Warna teks putih untuk kontras */
+    }
+    /* Tambahkan border pada sel */
+    
+  
+  
+
+   
+.table th:last-child, .table td:last-child {
+    border-right: none; /* Menghapus garis vertikal di kolom terakhir */
+}
+@media (max-width: 768px) {
+    body {
+        font-size: 0.8em; /* Ukuran font lebih kecil di perangkat kecil */
+    }
+    .card {
+        width: 95%; /* Lebar card lebih besar di perangkat kecil */
+    }
+    .table th, .table td {
+        padding: 5px; /* Padding lebih kecil di perangkat kecil */
+    }
+}
+.bg-purple {
+        background-color:  #ff80ff; /* Warna ungu */
+        color: black; /* Warna teks putih untuk kontras */
+        
+    }
+    .bg-custom {
+    background-color:  #ece9d8; /* Warna abu-abu */
+}
+.navbar-brand, .nav-link, .navbar-text {
+  font-size: 0.85em !important; /* Mengatur ukuran font untuk elemen di dalam nav */
+    }
+    .bg-header{
+    font-size: 0.84em;
+  background-color: #DFE3EB;
+}
+.table th{
+  color: #ffffff;
+  border: none;
+  background-image: url('back2 (2).gif');
+  height: 30px;
+  vertical-align: middle;
+ 
+}
+.no-margin {
+        margin: 0; /* Menghapus margin */
+        text-align: center;
+    }
+    .normal-font {
+        font-weight: 0.7 em; /* Mengatur font-weight menjadi normal */
+    }
+    .alert-note{
+      background-color: #FFFFCC
+    }
+    .font-large {
+    font-size: 1.7em; /* Ukuran font untuk Sarjana */
+    color: dodgerblue; /* Warna untuk Sarjana */
+}
+
+.font-small {
+    font-size: 0.8em; /* Ukuran font untuk Tahap */
+    color: black; /* Warna untuk Tahap */
+}
+.btn-modif{
+  background-color: #DFE3EB;
+  border-radius: 0;
+  font-size: 0.8em;
+}
+.GridStyle {
+    background-color: #ffffff;
+    background-image: url(grey-bar.gif);
+    background-repeat: repeat-x;
+    background-attachment: scroll;
+    background-position: 0pt 0pt;
+    border-top-style: solid;
+    border-right-style: solid;
+    border-bottom-width: 1px;
+    border-left-style: solid;
+    border-top-color: #d2d2d2;
+    border-right-color: #d2d2d2;
+    border-bottom-color: #d2d2d2;
+    border-left-color: #d2d2d2;
+    border-top-width: 0pt;
+    border-right-width: 1px;
+    border-bottom-style: solid;
+    border-left-width: 1px;
+    padding-top: 16px;
+    padding-right: 11px;
+    padding-bottom: 13px;
+    padding-left: 14px;
+    margin-bottom: 15px;
+    display: inline-block;
+    position: relative;
+    border-collapse: collapse;
+    -moz-border-radius: 5px;
+}
+.table tr, .table td {
+    border-bottom: none !important;
+}
+.table th {
+    border-bottom: none !important; /* Menghapus border di bagian bawah */
+}
+.navbar-main {
+  color: #ffffff;
+    background-image: url('back2 (1).gif');
+  border: none;
+ 
+  font-size: 1.1em;
+  font-weight: bold;
+  height: 22px;
+    }
+    .navbar-custom .navbar-brand,
+    .navbar-custom .nav-link {
+      color: white; /* Warna teks */
+    }
+    .navbar-custom .nav-link:hover {
+      color: #ccc; /* Warna teks saat hover */
+    }
+    .dropdown-menu {
+      width: 230px; /* Atur lebar dropdown menu */
+    
+    font-size: 0.9em; /* Atur ukuran font */
+      background-color: #0d6efd; /* Warna latar belakang dropdown */
+      border-radius: 0;
+    }
+    .dropdown-item {
+      color: white; /* Warna teks dropdown */
+      
+    font-size: 0.9em; /* Atur ukuran font untuk setiap item */
+    }
+    .dropdown-item:hover {
+      background-color: #1e90ff; /* Warna latar belakang saat hover */
+      color: white;
+    }
+    .navbar {
+  padding-top: 0.30rem !important;
+  padding-bottom: 0.30rem !important;
+}
+.navbar-brand, .nav-link {
+  font-size: 0.9em; /* Mengurangi ukuran font */
+  line-height: 1.2; /* Mengurangi tinggi baris */
+}
+
+.nav-link {
+  padding-top: 0.25rem; /* Mengurangi padding atas */
+  padding-bottom: 0.25rem; /* Mengurangi padding bawah */
+}
+.navbar-custom {
+    padding-top: 0 !important; /* Hilangkan padding atas */
+    padding-bottom: 0 !important; /* Hilangkan padding bawah */
+    margin: 0 !important; /* Hilangkan margin */
+    align-items: flex-start !important; /* Mepet ke atas */
+}
+
+.navbar-brand, .nav-link, .navbar-text {
+    font-size: 0.8em !important; /* Kurangi ukuran font */
+    line-height: 1 !important; /* Kurangi line height */
+    padding-top: 0 !important; /* Hilangkan padding atas */
+    padding-bottom: 0 !important; /* Hilangkan padding bawah */
+    margin: 0 !important; /* Hilangkan margin */
+}
+
+.container-fluid {
+    padding-top: 0 !important; /* Hilangkan padding atas */
+    padding-bottom: 0 !important; /* Hilangkan padding bawah */
+    margin: 0 !important; /* Hilangkan margin */
+}
+.d-flex.align-items-center {
+    margin-left: -10px; /* Geser ke kiri */
+}
+
+.navbar-brand {
+    margin-left: -10px; /* Sesuaikan margin kiri */
+}
+
+.nav-link {
+    margin-left: -10px; /* Sesuaikan margin kiri */
+}
+.dropdown-submenu {
+  position: relative;
+}
+
+.dropdown-submenu .dropdown-submenu-right {
+  position: absolute;
+  top: 0;
+  left: 100%;
+  background-color:  #0d6efd;;
+  
+  padding: 10px;
+  display: none;
+  width: 250px;
+}
+
+.dropdown-submenu .dropdown-submenu-right li {
+  margin-bottom: 10px;
+}
+
+.dropdown-submenu .dropdown-submenu-right li:last-child {
+  margin-bottom: 0;
+}
+
+.dropdown-submenu .dropdown-submenu-right a {
+  color: white;
+  text-decoration: none;
+}
+
+.dropdown-submenu .dropdown-submenu-right a:hover {
+  color: #ccc;
+  text-decoration: none;
+}
+
+.dropdown-submenu:hover .dropdown-submenu-right {
+  display: block;
+}
+</style>
+<div id="banner">
+<img src="header.png" alt="Gambar 1" style="width: 1000px; height: auto; margin-right: 10px; margin-left : 10px;">
+</div>
+
+
+
+  
+
+  
+ 
+
+
+
+<div align="center">
+	<br>
+  <table width="700" cellpadding="0" cellspacing="0">
+    <tbody><tr>
+      <td><img src="images/selamatdatang.gif"></td>
+    </tr>
+    <tr>
+      <td align="center">
+	  	  
+
+		
+
+      
+      	  <table width="700" cellpadding="0" cellspacing="0">
+          <tbody><tr>
+            <td width="10"><img src="images/left_corner.gif" width="10" height="10"></td>
+            <td width="680" bgcolor="#C1C1C1" style="background-image:url(images/upper_accent.gif);"> </td>
+            <td width="10"><img src="images/right_corner.gif" width="10" height="10"></td>
+          </tr>
+          <tr>
+            <td height="50" colspan="3" valign="top" style="padding:5px;background-color:#ccccff;width:700px">
+							
+			
+				<p>&nbsp;</p>
+<p style="text-align: center;"><span style="font-size: medium;"><strong>PENGUMUMAN NILAI MASUK</strong></span></p>
+<ul>
+<li><span style="font-size: small;">Mulai semester genap 2018 diberlakukan rentang nilai yang baru sesuai Peraturan Akademik 2018 yaitu:&nbsp;</span><span style="font-size: small;">&nbsp;</span><span style="font-size: small;">&nbsp;</span></li>
+</ul>
+<blockquote>
+<ul>
+<li><span style="font-size: small;">Nilai angka (86-100) : Nilai A / Istimewa</span></li>
+<li><span style="font-size: small;">Nilai angka (76-85) : Nilai AB / Baik Sekali</span></li>
+<li><span style="font-size: small;">Nilai angka (66-75) : Nilai B / Baik</span></li>
+<li><span style="font-size: small;">Nilai angka (61-65) : Nilai BC / Cukup Baik</span></li>
+<li><span style="font-size: small;">Nilai angka (56-60) : Nilai C / Cukup</span></li>
+<li><span style="font-size: small;">Nilai angka (41-55) : Nilai D / Kurang</span></li>
+<li><span style="font-size: small;">Nilai angka (0-40) : Nilai E / Kurang Sekali </span></li>
+</ul>
+</blockquote>
+<ul>
+</ul>
+<hr>
+<p style="text-align: center;"><span style="font-size: medium;"><strong>IPD SEMESTER GASAL 2024/2025</strong></span></p>
+<ul>
+<li><span style="font-size: small;">Diumumkan kepada Mahasiswa S3, S2, S1, Diploma Empat, dan Diploma Tiga diwajibkan mengisi Kuesioner Index </span><span style="font-size: small;">Pengajaran Dosen (IPD) semester <strong>Gasal 2024/2025</strong> secara online melalui SIM Akademik untuk semua matakuliah yang diikuti kecuali Kerja Praktek/TA/Tesis/Disertasi</span></li>
+</ul>
+<ul>
+<li><span style="font-size: small;">Pengisian kuesioner dimulai tanggal <span style="color: #0000ff;"><strong>25 November 2024</strong></span>&nbsp;sampai dengan <span style="color: #0000ff;"><strong>18 Desember 2024 pukul 23.59 WIB<br></strong></span></span></li>
+</ul>
+<ul>
+<li><span style="font-size: small;">Mahasiswa yang masih punya tanggungan pengisian semester sebelumnya dapat mengisi dalam periode sekarang dengan mengubah setting <strong>tahun</strong> dan <strong>semester</strong> serta <strong>kurikulumnya</strong>. Pengubahan kurikulum ada pada menu <span style="text-decoration: underline;"><span style="color: #0000ff;">Proses &gt;&gt; Kurikulum Semester</span></span></span></li>
+</ul>
+<ul>
+<li><span style="font-size: small;"><strong><span style="color: #ff0000;">Untuk memastikan kuesioner telah selesai</span></strong>, periksa <strong><span style="color: #0000ff;">Riwayat IPD</span></strong></span><span style="font-size: small;">, apabila masih ada yang belum terisi/kurang akan ditunjukkan sisi mana yang belum/kurang.</span></li>
+</ul>
+<ul>
+<li><span style="font-size: small;">Ada baiknya anda <strong><span style="color: #0000ff;">simpan Riwayat IPD</span></strong> dalam tangkapan layar sebagai bukti anda telah selesai.</span></li>
+</ul>
+<ul>
+</ul>
+<ul>
+</ul>
+<ul>
+<li><span style="font-size: small;">Isian kuesioner dijamin kerahasiannya</span></li>
+</ul>
+<p>&nbsp;</p>
+<p><span style="font-size: small;">Direktorat Pendidikan - ITS<br></span></p>       			</td>
+          </tr>
+      </tbody></table>
+	  
+	  
+		<h2>Berikut modul-modul yang bisa anda akses: </h2>
+		
+		<div style="text-align:left;background-color:#efefef">
+		<div style="background-color:#DFE3EB;padding:5px;font-size:14px;font-weight:bold">Data</div><p style="padding-left:30px"><img src="images/bullet.gif"> <a href="data_update_biodata.php" style="font-size:13px;font-weight:bold">Verifikasi Biodata</a></p><p style="padding-left:30px"><img src="images/bullet.gif"> <a href="data_mhswisuda.php" style="font-size:13px;font-weight:bold">Update Data Wisuda</a></p><p style="padding-left:30px"><img src="images/bullet.gif"> <a href="list_ekivalensi.php" style="font-size:13px;font-weight:bold">Ekivalensi</a></p><div style="background-color:#DFE3EB;padding:5px;font-size:14px;font-weight:bold">Proses</div><p style="padding-left:30px"><img src="images/bullet.gif"> <a href="ipd_kuesionermk.php" style="font-size:13px;font-weight:bold">Kuesioner Dosen dan MK</a></p><p style="padding-left:30px"><img src="images/bullet.gif"> <a href="data_kur.php" style="font-size:13px;font-weight:bold">Kurikulum Semester</a></p><p style="padding-left:30px"><img src="images/bullet.gif"> <a href="list_frs.php" style="font-size:13px;font-weight:bold">Formulir Rencana Studi</a></p>
+    <div style="background-color:#DFE3EB;padding:5px;font-size:14px;font-weight:bold">Laporan</div>
+    <p style="padding-left:30px"><img src="images/bullet.gif"> <a href="xrep_transkrip_utama.php" style="font-size:13px;font-weight:bold">Transkrip</a></p>
+    <p style="padding-left:30px"><img src="images/bullet.gif"> <a href="xrep_transkrip_sementara.php" style="font-size:13px;font-weight:bold">Transkrip Sementara</a></p>
+    <p style="padding-left:30px"><img src="images/bullet.gif"> <a href="data_mhsakademik.php" style="font-size:13px;font-weight:bold">Akademik Mahasiswa</a></p>
+    <p style="padding-left:30px"><img src="images/bullet.gif"> <a href="xrep_kartumahasiswa.php" style="font-size:13px;font-weight:bold">KTM Virtual</a></p>
+    <p style="padding-left:30px"><img src="images/bullet.gif"> <a href="data_mhs.php" style="font-size:13px;font-weight:bold">Biodata Mahasiswa</a></p>
+    <p style="padding-left:30px"><img src="images/bullet.gif"> <a href="data_kuliah.php" style="font-size:13px;font-weight:bold">Perkuliahan Mahasiswa</a></p>
+    <p style="padding-left:30px"><img src="images/bullet.gif"> <a href="list_mhsmk.php" style="font-size:13px;font-weight:bold">Daftar Mhs &amp; Matakuliah</a></p>
+    <p style="padding-left:30px"><img src="images/bullet.gif"> <a href="list_mhsstatang.php" style="font-size:13px;font-weight:bold">Status Per Angkatan</a></p>
+    <p style="padding-left:30px"><img src="images/bullet.gif"> <a href="list_prasyarat.php" style="font-size:13px;font-weight:bold">Prasyarat Matakuliah</a></p>
+    <p style="padding-left:30px"><img src="images/bullet.gif"> <a href="list_peminatan.php" style="font-size:13px;font-weight:bold">Peminatan dan Pengayaan Matakuliah</a></p>
+    <p style="padding-left:30px"><img src="images/bullet.gif"> <a href="list_mhsjadwal.php" style="font-size:13px;font-weight:bold">Jadwal Kuliah Mhs</a></p>
+    <p style="padding-left:30px"><img src="images/bullet.gif"> <a href="data_nilaimhs.php" style="font-size:13px;font-weight:bold">Nilai Per Mhs</a></p>
+    <p style="padding-left:30px"><img src="images/bullet.gif"> <a href="data_nilaipersem.php" style="font-size:13px;font-weight:bold">Nilai Per Semester</a></p>
+    <p style="padding-left:30px"><img src="images/bullet.gif"> <a href="grafik_perkembangan_ipk_ips.php" style="font-size:13px;font-weight:bold">Perkembangan Nilai</a></p>
+    
+    <p style="padding-left:30px"><img src="images/bullet.gif"> <a href="xrep_cetak_khs.php" style="font-size:13px;font-weight:bold">Kartu Hasil Studi</a></p>
+
+
+    <div style="background-color:#DFE3EB;padding:5px;font-size:14px;font-weight:bold">Lain-Lain</div><p style="padding-left:30px"><img src="images/bullet.gif"> <a href="css_bio_puas.php" style="font-size:13px;font-weight:bold">Survei Kepuasan Mahasiswa</a></p><div style="background-color:#DFE3EB;padding:5px;font-size:14px;font-weight:bold">Ekivalensi</div><p style="padding-left:30px"><img src="images/bullet.gif"> <a href="ekivalensi_rekapitulasi_mhs.php" style="font-size:13px;font-weight:bold">Rekapitulasi Ekivalensi</a></p><div style="background-color:#DFE3EB;padding:5px;font-size:14px;font-weight:bold">Yudisium</div><p style="padding-left:30px"><img src="images/bullet.gif"> <a href="yudisium_nilai_bahasa_asing.php" style="font-size:13px;font-weight:bold">Unggah Nilai Bahasa Asing</a></p><div style="background-color:#DFE3EB;padding:5px;font-size:14px;font-weight:bold">SKPI</div><p style="padding-left:30px"><img src="images/bullet.gif"> <a href="skpi_draft.php" style="font-size:13px;font-weight:bold">Draft SKPI</a></p><p style="padding-left:30px"><img src="images/bullet.gif"> <a href="downloads/panduan_umum_pengisian_skpi.pdf" style="font-size:13px;font-weight:bold">Panduan Umum (pdf)</a></p><p style="padding-left:30px"><img src="images/bullet.gif"> <a href="downloads/user_manual_skpi_akad_mhs_v01.pdf" style="font-size:13px;font-weight:bold">Sebagai Mahasiswa (pdf)</a></p><div style="background-color:#DFE3EB;padding:5px;font-size:14px;font-weight:bold">Biaya Pendidikan</div><p style="padding-left:30px"><img src="images/bullet.gif"> <a href="data_historis_pembayaran.php" style="font-size:13px;font-weight:bold">Historis Pembayaran SPP</a></p><p style="padding-left:30px"><img src="images/bullet.gif"> <a href="data_tagihan_pendidikan.php" style="font-size:13px;font-weight:bold">Tagihan Biaya Pendidikan</a></p><p style="padding-left:30px"><img src="images/bullet.gif"> <a href="data_pembayaran_wisuda.php" style="font-size:13px;font-weight:bold">Pembayaran Wisuda</a></p><div style="background-color:#DFE3EB;padding:5px;font-size:14px;font-weight:bold">Surat Mahasiswa^</div><p style="padding-left:30px"><img src="images/bullet.gif"> <a href="surat_mahasiswa.php" style="font-size:13px;font-weight:bold">Layanan Surat Mahasiswa</a></p><p style="padding-left:30px"><img src="images/bullet.gif"> <a href="downloads/panduan_layanan_surat_mahasiswa.pdf" style="font-size:13px;font-weight:bold">Panduan Layanan (PDF)</a></p>		</div>
+	  
+	  </td>
+    </tr>
+  </tbody></table>
+</div>
+</body>
+</html>
